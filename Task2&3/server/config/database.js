c4de8b6fb3bb8da7fe6d537bfcf7b84284c2d5d5
MeghanaKotambari <meghanakotambari@gmail.com>
2026-03-12 17:56:46 +0530

    Completed Task 3

--- Task2&3/server/config/database.js
+++ Task2&3/server/config/database.js
@@ -2,7 +2,7 @@ const { default: mongoose } = require("mongoose");
 
 const connectDB = async (req, res) => {
   try {
-    await mongoose.connect(process.env.MONGO_URI).then(() => {
+    await mongoose.connect("mongodb://localhost:27017/yumaris").then(() => {
       console.log("MongoDB connection successful");
     });
   } catch (error) {
